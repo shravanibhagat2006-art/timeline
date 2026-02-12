9615e093c8e0b6c435d4ebcac07577559d281c75
shravs <shravani.bhagat2006@gmail.com>
2026-02-12 10:29:58 +0530

    made changes

--- timescript.js
+++ timescript.js
@@ -1,13 +1,29 @@
-const items = document.querySelectorAll(".timeline-item");
+function enterSite() {
+  document.getElementById("intro").style.display = "none";
+}
 
+function toggleItem(element) {
+  element.classList.toggle("active");
+}
+
+/* Floating Hearts */
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.innerHTML = "💖";
+  heart.classList.add("heart");
+  heart.style.left = Math.random() * 100 + "vw";
+  document.body.appendChild(heart);
+
+  setTimeout(() => heart.remove(), 4000);
+}, 1200);
+
+/* Scroll Progress */
 window.addEventListener("scroll", () => {
-  const trigger = window.innerHeight * 0.85;
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
 
-  items.forEach(item => {
-    const itemTop = item.getBoundingClientRect().top;
-
-    if (itemTop < trigger) {
-      item.classList.add("show");
-    }
-  });
+  const scrolled = (scrollTop / scrollHeight) * 100;
+  document.querySelector(".progress-bar").style.width = scrolled + "%";
 });
