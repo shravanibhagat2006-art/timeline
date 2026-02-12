1a34ff718770c96fdd4b85af5eced96f901bb8ed
shravs <shravani.bhagat2006@gmail.com>
2026-02-12 10:38:04 +0530

    changes made

--- timescript.js
+++ timescript.js
@@ -15,15 +15,4 @@ setInterval(() => {
   document.body.appendChild(heart);
 
   setTimeout(() => heart.remove(), 4000);
-}, 1200);
-
-/* Scroll Progress */
-window.addEventListener("scroll", () => {
-  const scrollTop = document.documentElement.scrollTop;
-  const scrollHeight =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-
-  const scrolled = (scrollTop / scrollHeight) * 100;
-  document.querySelector(".progress-bar").style.width = scrolled + "%";
-});
+}, 1500);
